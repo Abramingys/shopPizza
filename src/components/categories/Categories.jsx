2cce344b08a054d401036b4cf4e22dab64a4f6b0
Vladimir Abramovich <abramingys@gmail.com>
2023-09-05 15:42:32 +0300

    1

--- src/components/categories/Categories.jsx
+++ src/components/categories/Categories.jsx
@@ -4,7 +4,7 @@ import categories from '../../arr/categories.json';
 function Categories() {
     let [active, setActive] = React.useState();
     let [active1, setActive1] = React.useState(false);
-    console.log('hi');
+    console.log('hids');
 
     function get(index) {
         setActive(index)
